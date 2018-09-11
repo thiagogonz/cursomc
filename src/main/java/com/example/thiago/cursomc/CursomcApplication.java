eb6c40114f6b1fe5d161e862e5fe8deecdab129d
Thiago Gonzalez <thiagogonz@gmail.com>
2018-09-11 10:33:54 -0300

    Criando operacao de instanciacao e insercao no Banco

--- src/main/java/com/example/thiago/cursomc/CursomcApplication.java
+++ src/main/java/com/example/thiago/cursomc/CursomcApplication.java
@@ -1,12 +1,36 @@
 package com.example.thiago.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.thiago.cursomc.domain.Categoria;
+import com.example.thiago.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		
+	}
+	
+	
 }
